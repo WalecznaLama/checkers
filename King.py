ec35e2645ae2a8e1e68edd59c0de9777c397b49e
Rafał Kryński <rafal.krynski13@gmail.com>
2023-11-16 22:33:03 +0100

    important TODO, cleanup

--- King.py
+++ King.py
@@ -6,70 +6,6 @@ class King(Piece):
 		super().__init__(row, column, color, board)
 		self.representation = color.upper()
 
-	# def possible_moves(self):  # available moves on empty board
-	# 	moves_ru = []  # RightUp (A1 to H8)
-	# 	moves_rd = []  # RightDown (A8 to H1)
-	# 	moves_ld = []  # LeftDown (H8 to A1)
-	# 	moves_lu = []  # LeftUp (H1 to A8)
-	# 	r = self.row
-	# 	c = self.column
-	#
-	# 	for i in range(1, self.board.BOARD_SIZE):  # RightUp (A1 to H8)
-	# 		if r + i > self.board.BOARD_SIZE - 1 or c + i > self.board.BOARD_SIZE - 1:
-	# 			break
-	# 		else:
-	# 			moves_ru.append((i, i))
-	# 	for i in range(1, self.board.BOARD_SIZE):  # RightDown (A8 to H1)
-	# 		if r - i < 0 or c + i > self.board.BOARD_SIZE - 1:
-	# 			break
-	# 		else:
-	# 			moves_rd.append((-i, i))
-	# 	for i in range(1, self.board.BOARD_SIZE):  # LeftDown (H8 to A1)
-	# 		if r - i < 0 or c - i < 0:
-	# 			break
-	# 		else:
-	# 			moves_ld.append((-i, -i))
-	# 	for i in range(1, self.board.BOARD_SIZE):  # LeftUp (H1 to A8)
-	# 		if r + i > self.board.BOARD_SIZE - 1 or c - i < 0:
-	# 			break
-	# 		else:
-	# 			moves_lu.append((i, -i))
-	# 	return [moves_ru, moves_rd, moves_ld, moves_lu]
-	#
-	# def valid_moves(self):
-	# 	valid_moves = []
-	# 	moves = self.possible_moves()
-	# 	for direction in moves:
-	# 		for move in direction:
-	# 			if self.board.is_tile_empty((self.row+move[0], self.column+move[1])):
-	# 				valid_moves.append(move)
-	# 			else:
-	# 				break
-	# 	return valid_moves
-	#
-	# def valid_jumps(self):  # valid jumps with pose of beaten opponent
-	# 	valid_jumps = []
-	# 	moves = self.possible_moves()  # all possible piece moves, assuming that board is empty
-	# 	opponent = self.board.get_opponent_color()
-	# 	for direction in moves:
-	# 		hit_opponent = False  # opponent at tile
-	# 		beaten_pose = []
-	# 		valid_behind_moves = []  # valid moves behind beaten opponent
-	# 		for move in direction:
-	# 			move_tile = (self.row+move[0], self.column+move[1])
-	# 			if self.board.is_tile_empty(move_tile) and hit_opponent:  # empty tile behind opponent
-	# 				valid_behind_moves.append(move)
-	# 			elif self.board.get_tile_color(move_tile) == opponent:  # tile with opponent
-	# 				hit_opponent = True
-	# 				beaten_pose = move_tile
-	# 			elif hit_opponent:  # first not empty tile behind opponent
-	# 				break
-	# 		if hit_opponent:
-	# 			valid_jumps.append([beaten_pose, valid_behind_moves])
-	# 			beaten_pose = []
-	# 			valid_behind_moves = []
-	# 	return valid_jumps
-
 	def possible_moves(self, pose):  # available moves on empty board
 		moves_ru = []  # RightUp (A1 to H8)
 		moves_rd = []  # RightDown (A8 to H1)
@@ -111,21 +47,23 @@ class King(Piece):
 					break
 		return valid_moves
 
-	def valid_jumps(self, pose):  # valid jumps with pose of beaten opponent
+	def valid_jumps(self, pose):  # valid jumps with pose of beaten opponent  # TODO skip start pose in chain
 		valid_jumps = []
 		moves = self.possible_moves(pose)  # all possible piece moves, assuming that board is empty
 		opponent = self.board.get_opponent_color()
 		for direction in moves:
 			hit_opponent = False  # opponent at tile
-			beaten_pose = []
+			beaten_pose = None
+			beaten_pose_buff = None  # buffer for beaten pose - necessary because used in next loop
 			valid_behind_moves = []  # valid moves behind beaten opponent
 			for move in direction:
 				move_tile = (pose[0]+move[0], pose[1]+move[1])
 				if self.board.is_tile_empty(move_tile) and hit_opponent:  # empty tile behind opponent
 					valid_behind_moves.append(move)
+					beaten_pose = beaten_pose_buff
 				elif self.board.get_tile_color(move_tile) == opponent:  # tile with opponent
 					hit_opponent = True
-					beaten_pose = move_tile
+					beaten_pose_buff = move_tile
 				elif hit_opponent:  # first not empty tile behind opponent
 					break
 			if hit_opponent:
@@ -134,7 +72,7 @@ class King(Piece):
 				valid_behind_moves = []
 		return valid_jumps
 
-	def find_chain_jumps(self, current_position, beaten_pieces=None):  # TODO
+	def find_chain_jumps(self, current_position, beaten_pieces=None):
 		if beaten_pieces is None:
 			beaten_pieces = set()
 
@@ -143,15 +81,32 @@ class King(Piece):
 
 		for beaten_pose, moves_after_beating in valid_jumps:
 			if beaten_pose not in beaten_pieces:
-				beaten_pieces.add(beaten_pose)
+				new_beaten_pieces = beaten_pieces.copy()
+				new_beaten_pieces.add(beaten_pose)
 				for move in moves_after_beating:
 					new_position = (current_position[0] + move[0], current_position[1] + move[1])
-					further_jumps = self.find_chain_jumps(new_position, beaten_pieces.copy())
+					further_jumps = self.find_chain_jumps(new_position, new_beaten_pieces)
 
 					if further_jumps:
 						for chain, beaten_in_chain in further_jumps:
-							all_chains.append(([new_position] + chain, beaten_pieces.union(beaten_in_chain)))
+							all_chains.append(([new_position] + chain, beaten_in_chain))
 					else:
-						all_chains.append(([new_position], beaten_pieces))
+						all_chains.append(([new_position], new_beaten_pieces))
 
 		return all_chains
+
+	def find_longest_chain_jumps(self):
+		chains = self.find_chain_jumps(self.pose)
+		longest_chains = []
+		if not chains:  # empty
+			return None
+		else:  # find the longest chain
+			max_len = 0
+			for i in chains:
+				max_len = max(max_len, len(i[1]))
+
+		for i in chains:  # add the longest chains to return variable
+			if len(i[1]) == max_len:
+				longest_chains.append(i)
+
+		return longest_chains
--- Pawn.py
+++ Pawn.py
@@ -24,28 +24,6 @@ class Pawn(Piece):
                 valid_moves.append(move)
         return valid_moves
 
-    # def _possible_jump_moves(self):  # available jumps on empty board
-    #     possible_moves = []
-    #     all_jumps = ((2, 2), (-2, 2), (-2, -2), (2, -2))  # RightUp, RightDown, LeftDown, LeftUp
-    #     for i in range(len(all_jumps)):
-    #         if (0 <= self.row + all_jumps[i][0] <= self.board.BOARD_SIZE - 1
-    #                 and 0 <= self.column + all_jumps[i][1] <= self.board.BOARD_SIZE - 1):
-    #             possible_moves.append(all_jumps[i])
-    #     return possible_moves
-
-    # def valid_jumps(self):
-    #     valid_jumps = []
-    #     possible_jumps = self._possible_jump_moves()  # all possible piece jump positions, assuming that board is empty
-    #     opponent = self.board.get_opponent_color()
-    #     for jump in possible_jumps:
-    #         adjacent_move = (self.row + int(jump[0] / 2), self.column + int(jump[1] / 2))  # closest black tiles poses
-    #         # tile with opponent
-    #         if self.board.get_tile_color(adjacent_move) == opponent:
-    #             pose = (self.row + jump[0], self.column + jump[1])
-    #             if self.board.is_tile_empty(pose):  # empty tile behind opponent
-    #                 valid_jumps.append(jump)
-    #     return valid_jumps
-
     def _possible_jump_moves(self, pose):  # available jumps on empty board
         possible_moves = []
         all_jumps = ((2, 2), (-2, 2), (-2, -2), (2, -2))  # RightUp, RightDown, LeftDown, LeftUp
@@ -55,7 +33,7 @@ class Pawn(Piece):
                 possible_moves.append(all_jumps[i])
         return possible_moves
 
-    def valid_jumps(self, pose):
+    def valid_jumps(self, pose):  # TODO skip start pose in chain
         valid_jumps = []
         # all possible piece jump positions, assuming that board is empty
         possible_jumps = self._possible_jump_moves(pose)
@@ -67,31 +45,6 @@ class Pawn(Piece):
                 if self.board.is_tile_empty((pose[0] + jump[0], pose[1] + jump[1])):  # empty tile behind opponent
                     valid_jumps.append(jump)
         return valid_jumps
-
-    # def find_chain_jumps(self, current_position, beaten_pieces=None):
-    #     if beaten_pieces is None:
-    #         beaten_pieces = set()
-    #
-    #     valid_jumps = self.valid_jumps(current_position)
-    #     all_chains = []
-    #
-    #     for jump in valid_jumps:
-    #         new_position = (current_position[0] + jump[0], current_position[1] + jump[1])
-    #         adjacent_move = (int(jump[0] / 2), int(jump[1] / 2))
-    #         beaten_piece = (current_position[0] + adjacent_move[0], current_position[1] + adjacent_move[1])
-    #
-    #         if beaten_piece not in beaten_pieces:
-    #             beaten_pieces.add(beaten_piece)
-    #             further_jumps = self.find_chain_jumps(new_position, beaten_pieces.copy())
-    #
-    #             for chain in further_jumps:
-    #                 all_chains.append([new_position] + chain)
-    #                 # all_chains.append([jump] + chain)
-    #
-    #             if not further_jumps:
-    #                 all_chains.append([new_position])
-    #
-    #     return all_chains
 
     def find_chain_jumps(self, current_position, beaten_pieces=None):
         if beaten_pieces is None:
@@ -116,3 +69,19 @@ class Pawn(Piece):
                     all_chains.append(([new_position], beaten_pieces))
 
         return all_chains
+
+    def find_longest_chain_jumps(self):
+        chains = self.find_chain_jumps(self.pose)
+        longest_chains = []
+        if not chains:  # empty
+            return None
+        else:  # find the longest chain
+            max_len = 0
+            for i in chains:
+                max_len = max(max_len, len(i[1]))
+
+        for i in chains:  # add the longest chains to return variable
+            if len(i[1]) == max_len:
+                longest_chains.append(i)
+
+        return longest_chains
